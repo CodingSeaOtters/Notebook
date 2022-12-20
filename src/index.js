2b20fd0ea73bc3ae7db2f77c3055c446aca4466e
CodingSeaOtters <99719541+CodingSeaOtters@users.noreply.github.com>
2022-12-20 14:35:35 +0100

    getting to normal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-import ReactDOM from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById("root")).render(<h1>Hello, Stranger</h1>)
+import ReactDOM from "react-dom"
+
+ReactDOM.render(<h1>Hey, Stranger</h1>, document.getElementById("root"))
